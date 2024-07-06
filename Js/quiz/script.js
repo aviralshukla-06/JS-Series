b169a3bca7992a15756361b1711f1fca0b9ae964
Aviral Shukla <137806095+aviralshukla-06@users.noreply.github.com>
2024-07-06 18:06:22 +0530

    Add files via upload

---
+++ Js/quiz/script.js
@@ -0,0 +1,309 @@
+const questions = [
+    {
+        question: "How many days makes a week ?",
+        optionA: "10 days",
+        optionB: "14 days",
+        optionC: "5 days",
+        optionD: "7 days",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "How many players are allowed on a soccer pitch ?",
+        optionA: "10 players",
+        optionB: "11 players",
+        optionC: "9 players",
+        optionD: "12 players",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Who was the first President of USA ?",
+        optionA: "Donald Trump",
+        optionB: "Barack Obama",
+        optionC: "Abraham Lincoln",
+        optionD: "George Washington",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "30 days has ______ ?",
+        optionA: "January",
+        optionB: "December",
+        optionC: "June",
+        optionD: "August",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "How manay hours can be found in a day ?",
+        optionA: "30 hours",
+        optionB: "38 hours",
+        optionC: "48 hours",
+        optionD: "24 hours",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Which is the longest river in the world ?",
+        optionA: "River Nile",
+        optionB: "Long River",
+        optionC: "River Niger",
+        optionD: "Lake Chad",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "_____ is the hottest Continent on Earth ?",
+        optionA: "Oceania",
+        optionB: "Antarctica",
+        optionC: "Africa",
+        optionD: "North America",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Which country is the largest in the world ?",
+        optionA: "Russia",
+        optionB: "Canada",
+        optionC: "Africa",
+        optionD: "Egypt",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Which of these numbers is an odd number ?",
+        optionA: "Ten",
+        optionB: "Twelve",
+        optionC: "Eight",
+        optionD: "Eleven",
+        correctOption: "optionD"
+    },
+
+    {
+        question: `"You Can't see me" is a popular saying by`,
+        optionA: "Eminem",
+        optionB: "Bill Gates",
+        optionC: "Chris Brown",
+        optionD: "John Cena",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Where is the world tallest building located ?",
+        optionA: "Africa",
+        optionB: "California",
+        optionC: "Dubai",
+        optionD: "Italy",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "The longest river in the United Kingdom is ?",
+        optionA: "River Severn",
+        optionB: "River Mersey",
+        optionC: "River Trent",
+        optionD: "River Tweed",
+        correctOption: "optionA"
+    },
+
+
+    {
+        question: "How many permanent teeth does a dog have ?",
+        optionA: "38",
+        optionB: "42",
+        optionC: "40",
+        optionD: "36",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Which national team won the football World cup in 2018 ?",
+        optionA: "England",
+        optionB: "Brazil",
+        optionC: "Germany",
+        optionD: "France",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Which US state was Donald Trump Born ?",
+        optionA: "New York",
+        optionB: "California",
+        optionC: "New Jersey",
+        optionD: "Los Angeles",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "How man states does Nigeria have ?",
+        optionA: "24",
+        optionB: "30",
+        optionC: "36",
+        optionD: "37",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "____ is the capital of Nigeria ?",
+        optionA: "Abuja",
+        optionB: "Lagos",
+        optionC: "Calabar",
+        optionD: "Kano",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Los Angeles is also known as ?",
+        optionA: "Angels City",
+        optionB: "Shining city",
+        optionC: "City of Angels",
+        optionD: "Lost Angels",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "What is the capital of Germany ?",
+        optionA: "Georgia",
+        optionB: "Missouri",
+        optionC: "Oklahoma",
+        optionD: "Berlin",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "How many sides does an hexagon have ?",
+        optionA: "Six",
+        optionB: "Sevene",
+        optionC: "Four",
+        optionD: "Five",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "How many planets are currently in the solar system ?",
+        optionA: "Eleven",
+        optionB: "Seven",
+        optionC: "Nine",
+        optionD: "Eight",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Which Planet is the hottest ?",
+        optionA: "Jupitar",
+        optionB: "Mercury",
+        optionC: "Earth",
+        optionD: "Venus",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "where is the smallest bone in human body located?",
+        optionA: "Toes",
+        optionB: "Ears",
+        optionC: "Fingers",
+        optionD: "Nose",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "How many hearts does an Octopus have ?",
+        optionA: "One",
+        optionB: "Two",
+        optionC: "Three",
+        optionD: "Four",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "How many teeth does an adult human have ?",
+        optionA: "28",
+        optionB: "30",
+        optionC: "32",
+        optionD: "36",
+        correctOption: "optionC"
+    }
+]
+
+let index = 0;
+const queBox = document.getElementById('display-question');
+const optionLabels = [
+    document.getElementById('option-one-label'),
+    document.getElementById('option-two-label'),
+    document.getElementById('option-three-label'),
+    document.getElementById('option-four-label')
+];
+
+const options = document.querySelectorAll('.radio');
+const nextQuestion = document.getElementById('nextQue');
+
+
+
+function generateQuestion() {
+    const data = questions[index];
+    console.log(data);
+    queBox.innerText = `${data.question}`;
+    optionLabels[0].innerText = data.optionA
+    optionLabels[1].innerText = data.optionB
+    optionLabels[2].innerText = data.optionC
+    optionLabels[3].innerText = data.optionD
+}
+
+generateQuestion();
+
+
+
+const NextQuestion = nextQuestion.addEventListener('click', () => {
+    getAns();
+    if (index < questions.length - 1) {
+        index++;
+        generateQuestion();
+
+        if (index === questions.length - 1) {
+            nextQuestion.textContent = "Submit";
+            nextQuestion.classList.add('submit');
+            showScore();
+        }
+
+
+    }
+});
+
+function showScore() {
+    const container = document.querySelector('.game-quiz-container');
+    container.innerHTML = `<h1>Thank you for attempting the test</h1>
+                           <h2>Your score: ${playerScore}</h2>
+                           <p>Wrong Answers: ${wrongAnswers}</p>
+                           <p>Right Answers: ${playerScore / 10}</p>
+                           <p>Grade: ${(playerScore / (questions.length * 10)) * 100}%</p>`;
+}
+
+
+
+let playerScore = 0;
+let wrongAnswers = 0;
+function getAns() {
+    const data = questions[index];
+    let answered = false;
+    options.forEach(option => {
+        if (option.checked) {
+            answered = true;
+            if (option.value === data.correctOption) {
+                playerScore += 10;
+            } else {
+                wrongAnswers++;
+            }
+        }
+    });
+
+    if (!answered) {
+        alert('Please pick an option');
+    }
+}
+
+
+
+
+
+
